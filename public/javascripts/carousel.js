816f3beea3886af8896cdeff9e0915d6f75b6f36
alexvanhell <alejandrodnl.cv@hotmail.com>
2018-04-14 14:18:08 -0500

    Carousel funcional

--- public/javascripts/carousel.js
+++ public/javascripts/carousel.js
@@ -4,7 +4,15 @@ function Carousel(element, options) {
     this.currentImg = 0;
 
     if (options) { // undefined, null, 0, ''
-        this.speed = options.speed;
+        this.speed = options.speed || 2000;
+        this.repeatDelay = options.repeatDelay || 5000;
+        this.auto = options.auto;
+        /* 
+        if(options.auto) {
+            setInterval( () => {
+                this.next();
+            }, options.repeatDelay || 5000)
+        } */
     }
 }
 
@@ -12,6 +20,12 @@ Carousel.prototype.init = function () {
     console.log('Velocidad:', this.speed);
 
     this.drawIndicators();
+
+    if(this.auto) {
+        setInterval( () => {
+            this.next();
+        }, this.repeatDelay);
+    }
 }
 
 Carousel.prototype.drawIndicators = function () {
@@ -24,9 +38,50 @@ Carousel.prototype.drawIndicators = function () {
         indicatorsContainer.append('<i class="indicator"></i>');
     }
 
+    let _this = this;
+
+    indicatorsContainer.find('i.indicator').click( function() { 
+        let index = indicatorsContainer.find('i.indicator').index( $(this) );
+        //console.log(index);
+        _this.setIndex(index);
+    });
+    
+
     indicatorsContainer.find('i.indicator').eq(0).addClass('active');
 }
 
 Carousel.prototype.next = function () {
+    const slider = this.element.find('.carousel-slider');
+    const length = this.element.find('.carousel-item').length - 1;
 
+    console.log(length);
+
+    this.currentImg++;
+
+    if (this.currentImg > length) {
+        this.currentImg = 0;
+    }
+
+    /* slider.animate({
+        left: - (100 * this.currentImg) + '%'
+    }, this.speed); */
+    this.animate();
+}
+
+Carousel.prototype.setIndex = function(index) {
+    this.currentImg = index;
+
+    this.animate();
+}
+
+Carousel.prototype.animate = function(left) {
+    const slider = this.element.find('.carousel-slider');
+    const indicatorsContainer = this.element.find('.carousel-indicators');
+
+    slider.animate({
+        left: - (100 * this.currentImg) + '%'
+    }, this.speed);
+
+    indicatorsContainer.find('i.indicator').removeClass('active');
+    indicatorsContainer.find('i.indicator').eq(this.currentImg).addClass('active');
 }
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,11 +7,15 @@ console.log(prueba); */
 //let element = document.getElementById('carousel');
 let element = $('#carousel');
 
-let carousel = new Carousel(element);
+let carousel = new Carousel(element, { auto: true });
 
 /* console.log(carousel.speed); */
 
 carousel.init();
+
+/* setTimeout(function() {
+    carousel.next();
+}, 2000); */
 
 /* let chido = new Carousel();
 
