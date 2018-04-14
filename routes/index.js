f2c031b4db23a8adf72b57a6a297e29f8ad55f9d
alexvanhell <alejandrodnl.cv@hotmail.com>
2018-04-14 10:15:33 -0500

    Ruta para pagina de inicio

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/inicio', function (req, res, next) {
+  // Cargar vista inicio.pug
+  res.render('inicio', { title: 'Hola mundo', variable: 'Soy una variable xD <b>nigga</b>' });
 });
 
 module.exports = router;
